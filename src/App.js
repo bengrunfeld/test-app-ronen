37704b8352a6276ed71c6e4c57d7838a3e2e0d4a
bengrunfeld <bengrunfeld@proton.me>
2022-10-06 23:30:26 +0300

    Remove junk files and functions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import './App.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function App() {
-  const [state, setState] = useState()
+  const [state, setState] = useState();
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(data => setState(JSON.stringify(data)))
-  }, [])
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((data) => setState(JSON.stringify(data)));
+  }, []);
 
   return (
     <div className="App">
-     <h1>Hi Ronen</h1>
-    <p>Data: {state}</p>
+      <h1>Hi Ronen</h1>
+      <p>Data: {state}</p>
     </div>
   );
 }
