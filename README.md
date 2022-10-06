79ee17d6395b771e13d5b8d5cddfd915a8ff6f69
bengrunfeld <bengrunfeld@proton.me>
2022-10-06 23:24:02 +0300

    Init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Test App
+
+Tests whether you can make an API call from the browser. Spoiler - you can.
+
+## To run the app
+
+Clone it, run `npm i`, and then run `npm start`.
+
+Navigate to `localhost:3000` if your system doesn't automatically open a browser at that address.
+
+
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [state, setState] = useState()
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(data => setState(JSON.stringify(data)))
+  }, [])
+
+  return (
+    <div className="App">
+     <h1>Hi Ronen</h1>
+    <p>Data: {state}</p>
+    </div>
+  );
+}
+
+export default App;
